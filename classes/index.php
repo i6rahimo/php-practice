660e61a6718b28391cef7ee303ff21f8a11fdd45
i6rahim <i6rahim210@gmail.com>
2023-10-07 17:02:11 +0300

    "add"

--- classes/index.php
+++ classes/index.php
@@ -12,4 +12,86 @@ class SimpleClass
 
 
 $err = new SimpleClass();
-$err->displayVar();
+// $err->displayVar();
+
+
+
+class User 
+{
+    public int $id;
+    public ?string $name;
+
+    public function __construct(int $id, ?string $name)
+    {
+        $this->id = $id;
+        $this->name = $name;
+    }
+}
+
+$user = new User(123, "qwee");
+
+// var_dump($user->id);
+// var_dump($user->name);
+
+
+class Shape 
+{
+    public int $numberOfSides;
+    public string $name;
+
+    public function setNumberOfSides(int $numberOfSides): void 
+    {
+        $this->numberOfSides = $numberOfSides;
+    }
+    public function setName(string $name): void 
+    {
+        $this->name = $name;
+    }
+    public function getNumberOfSides(): int
+    {
+        return $this->numbrtOfSides;
+    }
+    public function getName(): string 
+    {
+        return $this->name;
+    }
+}
+
+$circle = new Shape();
+$circle->setName('circle');
+echo $circle->getName();
+echo $circle->getNumberOfSides();
+echo $circle->getNumberOfSides();
+
+
+$triangle = new Shape();
+$triangle->setName('triangle');
+$triangle->setNumberOfSides(3);
+var_dump($triangle->getName());
+var_dump($triangle->getNumberOfSides());
+
+
+class Test 
+{
+    public readonly string $prop;
+    public function __construct(string $prop) {
+        $this->prop = $prop;
+    }
+}
+
+$test = new Test('foobar');
+var_dump($test->prop);
+
+
+?>
+
+
+
+
+
+
+
+
+
+
+ 
